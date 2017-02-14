7e9022365181cbb8479873067179bf60a7587b34
Ben Wheatley <ben@kitsunesoftware.com>
2017-02-14 13:10:12 +0000

    Refactor and document

--- Runestone/GameModel.swift
+++ Runestone/GameModel.swift
@@ -42,7 +42,15 @@ enum TileType: Int {
 class Tile: SKLabelNode {
 	var type: TileType
 	var realPosition: Int2DPosition?
-	var highlighted = false
+	var highlighted: Bool = false {
+		didSet {
+			if highlighted {
+				fontColor = UIColor.red
+			} else {
+				fontColor = UIColor.white
+			}
+		}
+	}
 	
 	init(type: TileType) {
 		self.type = type
@@ -65,6 +73,7 @@ class GameModel {
 	var width: Int
 	var height: Int
 	var tiles: [Tile]
+	var currentSelection = Array<Tile>()
 	
 	init() {
 		width = 4
@@ -101,10 +110,15 @@ class GameModel {
 		}
 	}
 	
+	/// Returns the number of non-blank, non-blocking tiles in this game
 	func tileCount() -> Int {
 		return tiles.filter({[TileType.Blank, TileType.Blocking].contains($0.type)}).count
 	}
 	
+	/**
+	- parameter location: Location to test
+	- returns: Is the location **both** inside the game space **and** blank?
+	*/
 	func routable(location: Int2DPosition) -> Bool {
 		if location.x < -1 || location.y < -1 || location.x > width || location.y > height {
 			return false
@@ -114,5 +128,137 @@ class GameModel {
 		}
 		let index = location.y*width + location.x
 		return tiles[index].type==TileType.Blank
+	}
+	
+	/// If the user has selected enough tiles, remove them or cancel selection as appropriate
+	func processUserActions() {
+		// If we have enough selected to remove anything, have a go at removing things
+		if currentSelection.count == 2 {
+			tryToMatchAndRemoveTiles(currentSelection[0], currentSelection[1])
+		} else if currentSelection.count > 2 {
+			// If more than than two tiles are selected, there has been a problem!
+			for tile in tiles {
+				deselect(tile:tile)
+			}
+		}
+	}
+	
+	/// Deselects a tile and removes it from the list of selected tiles
+	func deselect(tile:Tile) {
+		tile.highlighted = false
+		if let index = currentSelection.index(of: tile) {
+			currentSelection.remove(at: index)
+		}
+	}
+	
+	/**
+	Takes to tiles and if:
+	1) they match
+	**and**
+	2) a route exists between them
+	
+	it removes the tiles from the game. If they cannot be removed, deselects the second tile.
+	*/
+	func tryToMatchAndRemoveTiles(_ firstTile: Tile, _ secondTile: Tile) {
+		if firstTile.type == secondTile.type && (route(fastOrPretty:.Pretty, from:firstTile, to:secondTile) != nil) {
+			firstTile.removeFromParent()
+			secondTile.removeFromParent()
+			firstTile.type = TileType.Blank
+			secondTile.type = TileType.Blank
+			for tile in tiles {
+				deselect(tile:tile)
+			}
+		} else {
+			deselect(tile:currentSelection[1])
+		}
+	}
+	
+	/// Set of directions within the game.
+	enum Direction {
+		case North
+		case East
+		case South
+		case West
+	}
+	
+	/// Set of search options within the game.
+	enum FastOrPretty {
+		case Fast
+		case Pretty
+	}
+	
+	/** Searches a route between two tiles.
+	
+	- parameter fastOrPretty: "Pretty" routes use the smallest number of bends, for example two tiles next to each other is a zero-bend path;
+	You don't want to use a "pretty" search every frame on every tile, or at least you *didn't* when this was running on an iPhone 4.
+	
+	- parameter from: The origin tile
+	- parameter to: The destination tile
+	
+	- returns: Optional array of Int2DPosition representing the steps on the route; nil if no route exists
+	*/
+	func route(fastOrPretty: FastOrPretty, from: Tile, to: Tile) -> [Int2DPosition]? {
+		let minRemainingBends = ( fastOrPretty==FastOrPretty.Fast ? 2 : 1 )
+		
+		for remainingBends in minRemainingBends..<2 {
+			for direction in [Direction.North, Direction.East, Direction.South, Direction.West] {
+				let result = route(from: from.realPosition!, to: to.realPosition!, direction: direction, remainingBends: remainingBends)
+				if (result != nil) {
+					return result
+				}
+			}
+		}
+		
+		return nil
+	}
+	
+	/** Recursive route finder, not to be called publicly.
+	
+	- parameter from: The origin tile
+	- parameter to: The destination tile
+	- parameter direction: The initial direction to be moving in from the origin tile
+	- parameter remainingBends: How many more times the route is allowed to bend
+	
+	- returns: Optional array of Int2DPosition representing the steps on the route; nil if no route exists with the specified requirements.
+	*/
+	private func route(from: Int2DPosition, to: Int2DPosition, direction: Direction, remainingBends: Int) -> [Int2DPosition]? {
+		// If we have turned too often, return nil
+		if remainingBends<0 {
+			return nil
+		}
+		
+		var nextFrom = from
+		switch direction {
+		case .North:
+			nextFrom.y -= 1
+		case .East:
+			nextFrom.x += 1
+		case .South:
+			nextFrom.y += 1
+		case .West:
+			nextFrom.x -= 1
+		}
+		
+		// If the next place we're going to look at is the destination, just return the destination as the last step on the route to the destination
+		if (nextFrom.x==to.x && nextFrom.y==to.y) {
+			return [to]
+		}
+		
+		// If there is no route, return nil
+		if routable(location: nextFrom)==false {
+			return nil
+		}
+		
+		// For each direction, recursively route from there to the destination. If the attempt succeeds, add the place we started from to the start of the route, and pop one level of the recursion
+		for nextDirection in [Direction.North, Direction.South, Direction.East, Direction.West] {
+			if let route = route(from: nextFrom, to: to,
+			                     direction: nextDirection,
+			                     remainingBends: (direction==nextDirection) ? remainingBends : remainingBends-1) {
+				return [from] + route
+			}
+		}
+		
+		// There was no route in any direction from here, so return nil
+		return nil
 	}
 }
--- Runestone/GameScene.swift
+++ Runestone/GameScene.swift
@@ -21,7 +21,6 @@ class GameScene: SKScene {
 	private var lastUpdateTime : TimeInterval = 0
 	
 	var gameModel = GameModel()
-	var currentSelection = Array<Tile>()
 	
 	var lblTiles = SKLabelNode(text: "keyTilesRemaining".localize())
 	
@@ -45,117 +44,6 @@ class GameScene: SKScene {
 		}
 	}
 	
-	/**
-	Takes to tiles and if:
-	1) they match
-	**and**
-	2) a route exists between them
-	
-	it removes the tiles from the game. If they cannot be removed, deselects the second tile.
-	*/
-	func tryToMatchAndRemoveTiles(_ firstTile: Tile, _ secondTile: Tile) {
-		if firstTile.type == secondTile.type && (route(fastOrPretty:.Pretty, from:firstTile, to:secondTile) != nil) {
-			firstTile.removeFromParent()
-			secondTile.removeFromParent()
-			firstTile.type = TileType.Blank
-			secondTile.type = TileType.Blank
-			for tile in gameModel.tiles {
-				deselect(tile:tile)
-			}
-		} else {
-			deselect(tile:currentSelection[1])
-		}
-	}
-	
-	/// Set of directions within the game.
-	enum Direction {
-		case North
-		case East
-		case South
-		case West
-	}
-	
-	/// Set of search options within the game.
-	enum FastOrPretty {
-		case Fast
-		case Pretty
-	}
-	
-	/** Searches a route between two tiles.
-	
-	- parameter fastOrPretty: "Pretty" routes use the smallest number of bends, for example two tiles next to each other is a zero-bend path;
-	You don't want to use a "pretty" search every frame on every tile, or at least you *didn't* when this was running on an iPhone 4.
-	
-	- parameter from: The origin tile
-	- parameter to: The destination tile
-	
-	- returns: Optional array of Int2DPosition representing the steps on the route; nil if no route exists
-	*/
-	func route(fastOrPretty: FastOrPretty, from: Tile, to: Tile) -> [Int2DPosition]? {
-		let minRemainingBends = ( fastOrPretty==FastOrPretty.Fast ? 2 : 1 )
-		
-		for remainingBends in minRemainingBends..<2 {
-			for direction in [Direction.North, Direction.East, Direction.South, Direction.West] {
-				let result = route(from: from.realPosition!, to: to.realPosition!, direction: direction, remainingBends: remainingBends)
-				if (result != nil) {
-					return result
-				}
-			}
-		}
-		
-		return nil
-	}
-	
-	/** Recursive route finder, not to be called publicly.
-	
-	- parameter from: The origin tile
-	- parameter to: The destination tile
-	- parameter direction: The initial direction to be moving in from the origin tile
-	- parameter remainingBends: How many more times the route is allowed to bend
-	
-	- returns: Optional array of Int2DPosition representing the steps on the route; nil if no route exists with the specified requirements.
-	*/
-	private func route(from: Int2DPosition, to: Int2DPosition, direction: Direction, remainingBends: Int) -> [Int2DPosition]? {
-		// If we have turned too often, return nil
-		if remainingBends<0 {
-			return nil
-		}
-		
-		var nextFrom = from
-		switch direction {
-		case .North:
-			nextFrom.y -= 1
-		case .East:
-			nextFrom.x += 1
-		case .South:
-			nextFrom.y += 1
-		case .West:
-			nextFrom.x -= 1
-		}
-		
-		// If the next place we're going to look at is the destination, just return the destination as the last step on the route to the destination
-		if (nextFrom.x==to.x && nextFrom.y==to.y) {
-			return [to]
-		}
-		
-		// If there is no route, return nil
-		if gameModel.routable(location: nextFrom)==false {
-			return nil
-		}
-		
-		// For each direction, recursively route from there to the destination. If the attempt succeeds, add the place we started from to the start of the route, and pop one level of the recursion
-		for nextDirection in [Direction.North, Direction.South, Direction.East, Direction.West] {
-			if let route = route(from: nextFrom, to: to,
-			                     direction: nextDirection,
-			                     remainingBends: (direction==nextDirection) ? remainingBends : remainingBends-1) {
-				return [from] + route
-			}
-		}
-		
-		// There was no route in any direction from here, so return nil
-		return nil
-	}
-	
     func touchDown(atPoint pos : CGPoint) {
 		let touchedNodes = self.nodes(at: pos)
 		
@@ -167,31 +55,15 @@ class GameScene: SKScene {
 				tile.run(action)
 				tile.highlighted = !tile.highlighted
 				if (tile.highlighted) {
-					tile.fontColor = UIColor.red
-					currentSelection.append(tile)
+					gameModel.currentSelection.append(tile)
 				} else {
-					deselect(tile:tile)
+					gameModel.deselect(tile:tile)
 				}
 			}
 		}
 		
-		if currentSelection.count == 2 {
-			tryToMatchAndRemoveTiles(currentSelection[0], currentSelection[1])
-		} else if currentSelection.count > 2 {
-			// If it's greater than 2 there has been a problem!
-			for tile in gameModel.tiles {
-				deselect(tile:tile)
-			}
-		}
+		gameModel.processUserActions()
     }
-	
-	/// Deselects a tile (internal state, ought to be refactored…) and removes it from the list of selected tiles
-	func deselect(tile:Tile) {
-		tile.fontColor = UIColor.white
-		if let index = currentSelection.index(of: tile) {
-			currentSelection.remove(at: index)
-		}
-	}
 	
     func touchMoved(toPoint pos : CGPoint) {
     }
