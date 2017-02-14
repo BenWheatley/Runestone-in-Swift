1fd204e76cfe8801b5accca239f8fe039b4e790a
Ben Wheatley <ben@kitsunesoftware.com>
2017-02-14 12:21:56 +0000

    Added documentation

--- Runestone/KSArrayUtil.swift
+++ Runestone/KSArrayUtil.swift
@@ -9,6 +9,11 @@
 import Foundation
 
 extension Array {
+	/**
+	Shuffles (as in a deck of cards) this array. Modifies existing array, does not return a duplicate.
+	
+	Each element of the array is swapped at random with another element. No guarantees whatsoever are provided regarding the resulting order, so derangements are unlikely.
+	*/
 	mutating func shuffle() {
 		for indexToSwitch in 0..<self.count {
 			let range = UInt32( self.count - indexToSwitch )
